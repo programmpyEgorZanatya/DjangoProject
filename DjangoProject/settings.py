b54bc9ce1ff2fe97211766c07d85555f7352c6b3
programmpyEgorZanatya <programmpython@bk.ru>
2023-01-21 12:57:03 +0300

    Занимались FrontEnd и переходами

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v@exucssj)zbcshv4$)ozh_arh+!*1jnpb(2k_ewls2zz%k1&3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
--- DjangoProject/urls.py
+++ DjangoProject/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from MainApp import views
 
 urlpatterns = [
-    path('',views.home),
-    path('about/',views.about),
+    path('',views.home, name="home"),
+    path('about/',views.about, name="about"),
     path('items_list/',views.items_list, name="items-list"),
     path('item/<int:id>',views.item_page, name="item_page"),
 
