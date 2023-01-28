21e58d80c9ef0d489752f204a2bb6477997d12e0
programmpyEgorZanatya <programmpython@bk.ru>
2023-01-28 13:01:35 +0300

    РаботаСоStatic

--- DjangoProject/urls.py
+++ DjangoProject/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from MainApp import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('',views.home, name="home"),
@@ -8,4 +10,4 @@ urlpatterns = [
     path('items_list/',views.items_list, name="items-list"),
     path('item/<int:id>',views.item_page, name="item_page"),
 
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
